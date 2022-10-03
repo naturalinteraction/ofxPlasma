838ad80700e6f9b15d918c191c71acc45b4b2a21
Alessandro Valli <naturalinteract@gmail.com>
2022-10-03 14:12:51 +0200

    example works

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -3,13 +3,57 @@
 //--------------------------------------------------------------
 void testApp::setup()
 {
+    // the ofxPlasma class encapsulates a 'Hose'
+    // here we participate in a pool (which must exist)
     plasma.setup("test-pool");
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button)
 {
-    // todo: deposit a protein
+    // here we deposit a protein
+    Protein protein(make_descrip("test-protein"),
+                    make_ingests(   make_ingest("x", 67.7),
+                                    make_ingest("n", 5),    // some ingest we will not metabolize
+                                    make_ingest("key", "hello world"),
+                                    NULL)
+                                );
+    printf("%d\n", plasma.putProtein(protein));
+}
+
+//--------------------------------------------------------------
+void testApp::update()
+{
+    // here we show how we listen for proteins deposited in the pool
+    for (;;)
+    {
+        Protein p = plasma.getNextProtein();
+        if (p.IsNull ())
+        {
+            // say("no protein");
+            break;
+        }
+
+        if (! plasma.proteinDescripIs(p, "test-protein"))
+        {
+            printf("no test protein");
+            // p.Spew (std::cout);
+            // std::cout.flush ();
+        }
+        else  // it is a test protein
+        {
+            float x = plasma.extractIngestAsFloat(p, "x");
+            string val = plasma.extractIngestAsString(p, "key");
+            printf("%f %s\n", x, val.c_str());
+        }
+    }  // for
+}
+
+//--------------------------------------------------------------
+void testApp::exit()
+{
+    // here we withdraw the hose from the pool
+    plasma.shutdown();
 }
 
 //--------------------------------------------------------------
@@ -32,9 +76,6 @@ void testApp::gotMessage(ofMessage msg)  {}
 
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo)  {}
-
-//--------------------------------------------------------------
-void testApp::update()  {}
 
 //--------------------------------------------------------------
 void testApp::draw()  {}
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -19,6 +19,7 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-    
-    ofxPlasma plasma;
+    	void exit();
+
+    	ofxPlasma plasma;
 };
