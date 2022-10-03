f8b208bcd6487e377528e484706a01c2617eee74
Alessandro Valli <naturalinteract@gmail.com>
2022-10-03 13:39:13 +0200

    example placeholder

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-todo
+ofxPlasma
+=========
 
+An openFrameworks addon to integrate Oblong Industries' Plasma.
 
 Do you describe what your project does in your description and README?
 Do you list what operating systems you are supporting?
@@ -7,3 +9,18 @@ Do you have example projects?
 Do your examples compile against the latest openFrameworks distribution downloaded from the of website?
 Did you give credit and links to whoever wrote the original code your addon includes?
 Are you clear about how your addon is licensed? Be straightforward if you don't want people to use it commercially. Also make sure that your dependent libraries have licenses that you are respecting.
+
+
+
+Dependency
+-----------
+- ...
+
+Support
+-------------------------------
+- Tested on Ubuntu 20.04.5 LTS + openFrameworks 0.11.2-master
+- Supported types
+  - int
+  - float
+  - Str for strings
+  - ...
---
+++ example/src/ofApp.cpp
@@ -0,0 +1,43 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup()
+{
+    plasma.setup("test-pool");
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button)
+{
+    // todo: deposit a protein
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key)  {}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y)  {}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button)  {}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button)  {}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h)  {}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg)  {}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo)  {}
+
+//--------------------------------------------------------------
+void testApp::update()  {}
+
+//--------------------------------------------------------------
+void testApp::draw()  {}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key)  {}
